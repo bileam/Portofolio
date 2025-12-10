f2eced9e7854b1a1002978c338ce54cb3f2072f3
bileam mangalla <bileammangalla12@gmail.com>
2025-12-10 18:02:09 +0700

    10 12 2025, 18:02

--- src/Components/Latihan.jsx
+++ src/Components/Latihan.jsx
@@ -97,7 +97,7 @@ const Latihan = () => {
                    : "translate-y-full "
                } 
 
-            md:group-hover:delay-300 delay-0 duration-700 absolute inset-0 transition-transform ease-in-out  bg-purple-400/20 rounded-2xl `}
+            md:group-hover:delay-300 delay-0 duration-700 absolute inset-0 transition-transform ease-in-out  bg-black/30`}
               >
                 <div className="relative w-full h-full flex-col flex items-center p-3 justify-center">
                   <img src={item.gambar} alt="" className="w-20" />
@@ -110,122 +110,6 @@ const Latihan = () => {
             </div>
           </div>
         ))}
-
-        {/* dua */}
-        {/* <div
-          onClick={() => handleClik(2)}
-          style={{ backgroundImage: `url(${gambar})` }}
-          className={`md:w-[50%] w-full  overflow-hidden relative group      h-80    dark:bg-f-text self-center z-1`}
-        >
-          <div className="flex flex-col justify-end h-full relative ">
-            <div
-              className={`absolute text-[4rem]
-              ${
-                cleck === 2
-                  ? "scale-100 transition-all duration-500 ease-in-out"
-                  : ""
-              }
-               md:group-hover:scale-125  transition-all duration-700 -translate-y-[150%] text-purple-400 group-hover:text-shadow-purple   w-full flex justify-center`}
-            >
-              <h1 className="md:hidden   text-[3rem] w-100 flex justify-center  ">
-                {cleck === 2 ? (
-                  <span className={`shadow-2xl shadow-purple-400 rounded-full`}>
-                    Omaigattt!!!
-                  </span>
-                ) : (
-                  <span> Don't Cleck</span>
-                )}
-              </h1>
-              <h1 className="hidden md:block">Hover Me</h1>
-            </div>
-            <div
-              className={`md:group-hover:-translate-y-100 ${
-                cleck === 2 ? "-translate-y-100 delay-0 " : "delay-300"
-              }  md:group-hover:delay-0  flex flex-col items-center pb-5  md:delay-300
-              transition-all  duration-700 ease-in-out`}
-            >
-              <h1 className=" font-mono relative text-[1.5rem]">React Js</h1>
-            </div>
-            <div
-              className={`md:group-hover:translate-y-0 
-               md:translate-y-full 
-               ${cleck === 2 ? "translate-y-0 delay-300" : "translate-y-full "} 
-
-            md:group-hover:delay-300 delay-0 duration-700 absolute inset-0 transition-transform ease-in-out  bg-purple-400/20 rounded-2xl `}
-            >
-              <div className="relative w-full h-full flex-col flex items-center p-3 justify-center">
-                <img src={react} alt="" className="w-20" />
-                <h1 className="text-[1.5rem]">React Js</h1>
-                <p className="text-justify max-w-[95%] overflow-hidden text-[0.9rem]">
-                  React.js adalah library JavaScript yang digunakan untuk
-                  membangun user interface (UI) pada aplikasi web. React dibuat
-                  oleh Facebook (sekarang Meta) dan sangat populer karena cepat,
-                  fleksibel, dan mudah digunakan. Dengan React, kamu dapat
-                  membuat tampilan web berupa komponen—bagian-bagian kecil UI
-                  yang bisa digunakan ulang, seperti tombol, card, navbar, form,
-                  dll.
-                </p>
-              </div>
-            </div>
-          </div>
-        </div> */}
-        {/* tiga */}
-        {/* <div
-          onClick={() => handleClik(3)}
-          style={{ backgroundImage: `url(${gambar})` }}
-          className={`md:w-[50%] w-full  overflow-hidden relative group      h-80    dark:bg-f-text self-stars z-1`}
-        >
-          <div className="flex flex-col justify-end h-full relative ">
-            <div
-              className={`absolute text-[4rem]
-              ${
-                cleck === 3
-                  ? "scale-100 transition-all duration-500 ease-in-out"
-                  : ""
-              }
-               md:group-hover:scale-125  transition-all duration-700 -translate-y-[150%] text-purple-400 group-hover:text-shadow-purple   w-full flex justify-center`}
-            >
-              <h1 className="md:hidden   text-[3rem] w-100 flex justify-center  ">
-                {cleck === 3 ? (
-                  <span className={`shadow-2xl shadow-purple-400 rounded-full`}>
-                    Omaigattt!!!
-                  </span>
-                ) : (
-                  <span> Don't Cleck</span>
-                )}
-              </h1>
-              <h1 className="hidden md:block">Hover Me</h1>
-            </div>
-            <div
-              className={`md:group-hover:-translate-y-100 ${
-                cleck === 3 ? "-translate-y-100 delay-0 " : "delay-300"
-              }  md:group-hover:delay-0  flex flex-col items-center pb-5  md:delay-300
-              transition-all  duration-700 ease-in-out`}
-            >
-              <h1 className=" font-mono relative text-[1.5rem]">TailwindCss</h1>
-            </div>
-            <div
-              className={`md:group-hover:translate-y-0 
-               md:translate-y-full 
-               ${cleck === 3 ? "translate-y-0 delay-300" : "translate-y-full "} 
-
-            md:group-hover:delay-300 delay-0 duration-700 absolute inset-0 transition-transform ease-in-out  bg-purple-400/20 rounded-2xl `}
-            >
-              <div className="relative w-full h-full flex-col flex items-center p-3 justify-center">
-                <img src={tailwindcss} alt="" className="w-20" />
-                <h1 className="text-[1.5rem]">TailwindCss</h1>
-                <p className="text-justify max-w-[95%] overflow-hidden text-[0.9rem]">
-                  Tailwind CSS adalah utility-first CSS framework yang
-                  menyediakan kelas-kelas kecil siap pakai untuk membuat
-                  tampilan website secara cepat dan fleksibel. Berbeda dengan
-                  framework lain seperti Bootstrap yang menyediakan komponen
-                  jadi (navbar, card, modal), Tailwind memberi kamu kebebasan
-                  penuh melalui kelas-kelas utility
-                </p>
-              </div>
-            </div>
-          </div>
-        </div> */}
       </div>
     </div>
   );
