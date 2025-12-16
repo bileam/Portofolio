40c294b745c0c73bf70240fda3ff98902434e162
bileam mangalla <bileammangalla12@gmail.com>
2025-12-16 18:37:17 +0700

    burger

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -73,7 +73,7 @@ const Navbar = ({ isDark, setDark }) => {
             >
               <div
                 className={`border w-5 dark:border-t-fonta ${
-                  click ? "translate-y-1 rotate-45" : "rotate-0 translate-0"
+                  click ? "translate-y-2 rotate-45" : "rotate-0 translate-0"
                 } transition-all duration-700`}
               ></div>
               <div
